ee8448dce8983fabaf67fb116664f5114343732f
Aditya Sharma <67048929+extinctsion@users.noreply.github.com>
2024-10-02 07:23:01 +0000

    added file checks

--- Lychrel.py
+++ Lychrel.py
@@ -9,10 +9,12 @@
 DO NOT REMOVE THIS COMMENT AS THE CODE BELOW INCREASE THE RECUSION LIMIT
 THIS MAY ALSO RESULT TO TOTAL CRASH OF THE SYSTEM. IF YOUR PC IS STRONG ENOUGH TO TACKLE
 RECURSION, FEEL FREE TO USE THE CODE BY REMOVING THE COMMENTS.
-
-#import sys
-#sys.setrecursionlimit(1500)
 """
+import sys
+sys.setrecursionlimit(1500000)
+file_name = f"lychrel_numbers.txt"
+file = open(file_name, "w")
+
 #function to solve the lychrel number
 def prob(num):
         temp=num
@@ -23,8 +25,12 @@ def prob(num):
             num=num//10
         if(temp==rev):
             print(f"At last! we found a palindrome which is {temp}")
+            result = f"At last! we found a palindrome which is {temp}\n"
+            file.write(result)
         else:
             print(f"The number {temp} isn't a palindrome!")
+            result = f"The number {temp} isn't a palindrome!\n"
+            file.write(result)
             again = temp + reverse(temp)
             prob(again)
 
@@ -42,6 +48,12 @@ def reverse(num):
 
 
 #driver code
-num = int(input("Enter a two digit number or more"))
+num = int(input("Enter a two digit number or more\n"))
+# for i in range(1,100):
+#     print(f"\n\nInput number is {i}\n\n")
+#     result = f"\n\nInput number is {i}\n\n"
+#     file.write(result)
+#     prob(i)
 prob(num)
+file.close()
 
